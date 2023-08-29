624c83ec50305fadf6b71e308b8686f9bd4ffd84
Ricardo Garcia <ragarcia@caltech.edu>
2023-08-29 14:01:13 -0400

    Comment was appearing as code in google colab

--- flows/helper_functions.py
+++ flows/helper_functions.py
@@ -23,10 +23,6 @@ def get_correct_gradient_function(metric, integration_method=midpoint_rule):
 
 def smooth_f(x):
 # Smooth function on the real line that is zero for t<=0 and positive for t>0
-  #if x > 0.:
-  #  return np.exp(-1/x)
-  #else:
-  #  return 0.
   return jnp.where(x>0., jnp.exp(-1/x), 0.)
 
 def smoothstep(x, r1=0, r2=1):
@@ -36,10 +32,6 @@ def smoothstep(x, r1=0, r2=1):
   0 < smoothstep(x, r1, r2) < 1 for r1 < x < r2
   smoothstep(x, r1, r2) = 1 for x > r2
   '''
-  #o = r1
-  #s = 1/(r2-r1)
-  #t = smooth_f(s*(x-o))
-  #return t/(t+smooth_f(1-(s*(x-o))))
   y = (x-r1)/(r2-r1)
   f = 3*(y**2)-2*(y**3)
   return jnp.where(x<1,jnp.where(x<0, 0, f), 1)
