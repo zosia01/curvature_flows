cc85757cf1e47122205c47460e84b92c6c2b3a11
Ricardo Garcia <ragarcia@caltech.edu>
2023-08-29 10:59:49 -0400

    Added Smoothstep

--- flows/helper_functions.py
+++ flows/helper_functions.py
@@ -20,3 +20,24 @@ def plot_curve(curve, plotter=plt, closed_curve=True):
 def get_correct_gradient_function(metric, integration_method=midpoint_rule):
   p = (grad(lambda x: length_func(x, metric, integration_method)))
   return jit(lambda x: correct_gradient(p(x), x, metric))
+
+def smooth_f(x):
+# Smooth function on the real line that is zero for t<=0 and positive for t>0
+  #if x > 0.:
+  #  return np.exp(-1/x)
+  #else:
+  #  return 0.
+  return jnp.where(x>0., jnp.exp(-1/x), 0.)
+
+def smoothstep(x, r1=0, r2=1):
+  '''
+  smoothstep(x, r1, r2) is a smooth function of x satisfying
+  smoothstep(x, r1, r2) = 0 for x < r1
+  0 < smoothstep(x, r1, r2) < 1 for r1 < x < r2
+  smoothstep(x, r1, r2) = 1 for x > r2
+  '''
+  o = r1
+  s = 1/(r2-r1)
+  t = smooth_f(s*(x-o))
+  return t/(t+smooth_f(1-(s*(x-o))))
+
